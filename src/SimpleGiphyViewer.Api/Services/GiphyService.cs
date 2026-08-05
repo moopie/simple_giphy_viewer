7deec85e0c9e9f23550b8e92bd6ea649e93c0028
Anton Amirian <extuessl@gmail.com>
2026-08-05 21:28:33 +0300

    Cleanup GiphyService

--- src/SimpleGiphyViewer.Api/Services/GiphyService.cs
+++ src/SimpleGiphyViewer.Api/Services/GiphyService.cs
@@ -5,69 +5,58 @@ using SimpleGiphyViewer.Api.Models;
 
 namespace SimpleGiphyViewer.Api.Services;
 
-public class GiphyService(
-    IGiphyClient giphyClient,
-    IMemoryCache memoryCache,
-    ILogger<GiphyService> logger
-    ) : IGiphyService
+public class GiphyService(IGiphyClient giphyClient, IMemoryCache memoryCache, ILogger<GiphyService> logger)
+    : IGiphyService
 {
     public async Task<GiphyResponse> GetTrendingAsync()
     {
         var cacheKey = "giphy:trending";
-
         if (!memoryCache.TryGetValue<GiphyResponse>(cacheKey, out var res))
         {
-            logger.LogInformation("Got trending from cache");
-            
             if (res is null)
             {
                 memoryCache.Remove(cacheKey);
                 throw new EmptyResponseException(cacheKey);
             }
-            
+
+            logger.LogInformation("Got trending from cache");
             return res;
         }
 
         var response = await giphyClient.GetTrendingAsync();
-
         if (response is null)
         {
             throw new EmptyResponseException(cacheKey);
         }
-        
+
         logger.LogInformation("Storing trending in cache");
         memoryCache.Set(cacheKey, response, TimeSpan.FromMinutes(5));
-
         return response;
     }
-    
+
     public async Task<GiphyResponse> SearchAsync(string keyword)
     {
         var cacheKey = $"giphy:search:{keyword}";
-
         if (!memoryCache.TryGetValue<GiphyResponse>(cacheKey, out var res))
         {
-            logger.LogInformation("Got '{Keyword}' from cache", keyword);
-            
             if (res is null)
             {
                 memoryCache.Remove(cacheKey);
                 throw new EmptyResponseException(cacheKey);
             }
-            
+
+            logger.LogInformation("Got '{Keyword}' from cache", keyword);
             return res;
         }
 
         var response = await giphyClient.SearchAsync(keyword);
-
         if (response is null)
         {
             throw new EmptyResponseException(cacheKey);
         }
-        
+
         logger.LogInformation("Storing '{Keyword}' in cache", keyword);
         memoryCache.Set(cacheKey, response, TimeSpan.FromMinutes(5));
-
         return response;
     }
 }
