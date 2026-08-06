cc2c909ba540e0524b0ddc706388715012d336ef
Anton Amirian <extuessl@gmail.com>
2026-08-07 02:16:38 +0300

    added locks for cache saving

--- src/SimpleGiphyViewer.Api/Services/GiphyService.cs
+++ src/SimpleGiphyViewer.Api/Services/GiphyService.cs
@@ -1,4 +1,4 @@
-using Microsoft.Extensions.Caching.Memory;
+using System.Collections.Concurrent;
 using SimpleGiphyViewer.Api.Exceptions;
 using SimpleGiphyViewer.Api.Interfaces;
 using SimpleGiphyViewer.Api.Models;
@@ -8,10 +8,12 @@ namespace SimpleGiphyViewer.Api.Services;
 public class GiphyService(IGiphyClient giphyClient, ICacheService cacheService, ILogger<GiphyService> logger)
     : IGiphyService
 {
+    private readonly ConcurrentDictionary<string, SemaphoreSlim> _locks = new();
+
     public async Task<GiphyResponse> GetTrendingAsync(CancellationToken cancellationToken = default)
     {
         const string cacheKey = "giphy:trending";
-        
+
         var cached = await cacheService.GetAsync<GiphyResponse>(cacheKey, cancellationToken);
 
         if (cached is not null)
@@ -19,16 +21,36 @@ public class GiphyService(IGiphyClient giphyClient, ICacheService cacheService, 
             return cached;
         }
 
-        var response = await giphyClient.GetTrendingAsync(cancellationToken);
-        if (response is null)
-        {
-            throw new EmptyResponseException(cacheKey);
-        }
+        var sem = _locks.GetOrAdd(cacheKey, _ => new SemaphoreSlim(1, 1));
 
-        logger.LogInformation("Storing trending in cache");
-        
-        await cacheService.SetAsync(cacheKey, response, cancellationToken);
-        return response;
+        await sem.WaitAsync(cancellationToken);
+
+        try
+        {
+            // Second time in case the previous request cached the response
+            // while we were waiting
+            var ca = await cacheService.GetAsync<GiphyResponse>(cacheKey, cancellationToken);
+
+            if (ca is not null)
+            {
+                return ca;
+            }
+
+            var response = await giphyClient.GetTrendingAsync(cancellationToken);
+            if (response is null)
+            {
+                throw new EmptyResponseException(cacheKey);
+            }
+
+            logger.LogInformation("Storing trending in cache");
+
+            await cacheService.SetAsync(cacheKey, response, cancellationToken);
+            return response;
+        }
+        finally
+        {
+            sem.Release();
+        }
     }
 
     public async Task<GiphyResponse> SearchAsync(string keyword, CancellationToken cancellationToken = default)
@@ -37,7 +59,7 @@ public class GiphyService(IGiphyClient giphyClient, ICacheService cacheService, 
         ArgumentException.ThrowIfNullOrEmpty(kw);
 
         var cacheKey = $"giphy:search:{kw}";
-        
+
         var cached = await cacheService.GetAsync<GiphyResponse>(cacheKey, cancellationToken);
 
         if (cached is not null)
@@ -45,14 +67,31 @@ public class GiphyService(IGiphyClient giphyClient, ICacheService cacheService, 
             return cached;
         }
 
-        var response = await giphyClient.SearchAsync(kw, cancellationToken);
-        if (response is null)
-        {
-            throw new EmptyResponseException(cacheKey);
-        }
+        var sem = _locks.GetOrAdd(cacheKey, _ => new SemaphoreSlim(1, 1));
 
-        logger.LogInformation("Storing '{kw}' in cache", kw);
-        await cacheService.SetAsync(cacheKey, response, cancellationToken);
-        return response;
+        await sem.WaitAsync(cancellationToken);
+
+        try
+        {
+            var ca = await cacheService.GetAsync<GiphyResponse>(cacheKey, cancellationToken);
+            if (ca is not null)
+            {
+                return ca;
+            }
+
+            var response = await giphyClient.SearchAsync(kw, cancellationToken);
+            if (response is null)
+            {
+                throw new EmptyResponseException(cacheKey);
+            }
+
+            logger.LogInformation("Storing '{kw}' in cache", kw);
+            await cacheService.SetAsync(cacheKey, response, cancellationToken);
+            return response;
+        }
+        finally
+        {
+            sem.Release();
+        }
     }
 }
